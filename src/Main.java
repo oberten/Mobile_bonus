2a2a1e62dd38c4acf6983509e90b9d15dcc676a8
unknown <oberten@mail.ru>
2022-12-13 21:38:16 +0300

    changed

---
+++ src/Main.java
@@ -0,0 +1,25 @@
+public class Main {
+    public static void main(String[] args) {
+        int balanc = 500;
+        int amount = 300;
+        int totalBalanc = balanc + amount;
+
+        if (totalBalanc > 1000) {
+            int bonus = (totalBalanc / 100);
+            System.out.println(totalBalanc);
+            System.out.println(bonus);
+        } else  {
+            int bonus = (totalBalanc);
+            System.out.println(totalBalanc);
+            System.out.println(bonus = 0);
+        }
+    }
+}
+
+
+
+
+
+
+
+
